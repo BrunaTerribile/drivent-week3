dc036f1381f70a3ec599284d004bba05eb7812b4
Bruna Terribile <brunaterribile@gmail.com>
2023-02-06 11:00:33 -0300

    feat: get all hotels query

--- src/controllers/hotels-controller.ts
+++ src/controllers/hotels-controller.ts
@@ -1,9 +1,15 @@
 import { AuthenticatedRequest } from "@/middlewares";
 import { Response } from "express";
+import httpStatus from "http-status";
 import hotelService from "@/services/hotels-service";
 
 export async function getAll(req: AuthenticatedRequest, res: Response){
-
+    try {
+        const hotels = await hotelService.getHotels();
+        return res.status(httpStatus.OK).send(hotels);
+    } catch (error) {
+        return res.status(httpStatus.NOT_FOUND).send({});
+    }
 }
 
 export async function getHotelById(req: AuthenticatedRequest, res: Response){
--- src/services/hotels-service/index.ts
+++ src/services/hotels-service/index.ts
@@ -1,7 +1,13 @@
 import hotelRepository from "@/repositories/hotels-repository";
+import { notFoundError } from "@/errors";
 
 async function getHotels(){
+  const hotels = await hotelRepository.getHotels();
+  if (!hotels){
+    throw notFoundError();
+  }
 
+  return hotels;
 }
 
 async function getHotelById(){
